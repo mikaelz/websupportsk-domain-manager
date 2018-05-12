3849c467fe3a7552504527700c6ec5258399e8c1
Michal Zuber <info@nevilleweb.sk>
2018-05-12 11:17:07 +0200

    Finish DNS record creation

--- record_add.php
+++ record_add.php
@@ -1,34 +1,35 @@
 <?php
 
+namespace Websupport;
+
 if (empty($_GET['record_add']) || !filter_var(gethostbyname($_GET['record_add']), FILTER_VALIDATE_IP)) {
     echo '<div class="notice">Invalid domain. Couldn\'t resolve its IP.</div>';
 
     return;
 }
+
+if (isset($_POST['record_save'])) {
+    require __DIR__.'/record_save.php';
+}
 $domain = $_GET['record_add'];
 ?>
 <h2>Add new DNS record to <em><?php echo $domain; ?></em></h2>
-<form action="record_save.php" method="post">
+<form action="" method="post">
     <input type="hidden" name="domain" value="<?php echo $domain; ?>">
     <table class="zones" cellpadding="5" cellspacing="0" border="1">
         <tr>
             <th>Type</th>
             <td>
                 <select name="record_type" onchange="showTypeRows(this)">
-                    <option value="a">A</option>
-                    <option value="aaaa">AAAA</option>
-                    <option value="aname">ANAME</option>
-                    <option value="cname">CNAME</option>
-                    <option value="mx">MX</option>
-                    <option value="ns">NS</option>
-                    <option value="srv">SRV</option>
-                    <option value="txt">TXT</option>
+                    <?php foreach (Api::RECORD_TYPES as $type) : ?>
+                    <option value="<?php echo $type; ?>"><?php echo $type; ?></option>
+                    <?php endforeach; ?>
                 </select>
             </td>
         </tr>
         <tr>
             <th>Name</th>
-            <td><input type="text" name="record_name" value=""></td>
+            <td><input type="text" name="record_name" value="" autofocus="autofocus"></td>
         </tr>
         <tr>
             <th>Content</th>
@@ -52,7 +53,7 @@ $domain = $_GET['record_add'];
         </tr>
         <tr>
             <th>&nbsp;</th>
-            <td><button type="submit">Save new record</button></td>
+            <td><button type="submit" name="record_save">Save new record</button></td>
         </tr>
     </table>
 </form>
--- record_save.php
+++ record_save.php
@@ -2,18 +2,47 @@
 
 namespace Websupport;
 
-require __DIR__.'/bootstrap.php';
+$payload = [
+    'type' => $_POST['record_type'],
+    'name' => $_POST['record_name'],
+    'content' => $_POST['record_content'],
+    'ttl' => (int) $_POST['record_ttl'],
+];
 
-if (isset($_POST['record_add'])) {
-    $required[] = empty($_POST['record_type']) ? 'Type' : '';
-    $required[] = empty($_POST['record_name']) ? 'Name' : '';
-    $required[] = empty($_POST['record_content']) ? 'Content' : '';
-    $_SESSION['message'] = '<div class="error">Please provide: '.implode(', ', $required).'</div>';
+$required[] = empty($_POST['domain']) ? 'Domain' : '';
+$required[] = empty($_POST['record_type']) ? 'Type' : '';
+$required[] = empty($_POST['record_name']) ? 'Name' : '';
+$required[] = empty($_POST['record_content']) ? 'Content' : '';
 
-    header('Location: '.BASE_URL.'/record_add.php');
-    exit;
+switch (strtolower($_POST['record_type'])) {
+    case 'mx':
+        $required[] = empty($_POST['record_prio']) ? 'Priority' : '';
 
-    $ws = new Api($_SESSION['auth_token']);
+        $payload['prio'] = (int) $_POST['record_prio'];
+        break;
+    case 'srv':
+        $required[] = empty($_POST['record_prio']) ? 'Priority' : '';
+        $required[] = empty($_POST['record_port']) ? 'Port' : '';
+        $required[] = empty($_POST['record_weight']) ? 'Weight' : '';
+
+        $payload['prio'] = (int) $_POST['record_prio'];
+        $payload['port'] = (int) $_POST['record_port'];
+        $payload['weight'] = (int) $_POST['record_weight'];
+        break;
 }
 
-?>
+if (!empty($required[0])) {
+    echo '<div class="error">Please provide: '.implode(', ', $required).'</div>';
+
+    return;
+}
+
+$domain = $_POST['domain'];
+
+$ws = new Api($_SESSION['auth_token']);
+$response = $ws->request('/user/self/zone/'.$domain.'/record', $payload, 'POST');
+
+if (isset($response['status']) && 'success' === $response['status']) {
+    header('Location: '.BASE_URL.'/?domain='.$domain);
+    exit;
+}
