709259692a3ecd7d3dc58f672471097be315d866
Michal Zuber <info@nevilleweb.sk>
2018-05-12 09:25:45 +0200

    Set auth token in session if valid login

--- login.php
+++ login.php
@@ -6,11 +6,18 @@ require __DIR__.'/bootstrap.php';
 
 if (empty($_POST['ws_username']) || empty($_POST['ws_pass'])) {
     $_SESSION['message'] = '<div class="error">Please provide your login credentials</div>';
+
+    header('Location: '.BASE_URL);
+    exit;
 }
 
-$auth_token = '';
+$auth_token = base64_encode($_POST['ws_username'].':'.$_POST['ws_pass']);
 try {
     $ws = new Api($auth_token);
+    $ws_user = $ws->request('/user');
+    if ($ws_user['items'][0]['active']) {
+        $_SESSION['auth_token'] = $auth_token;
+    }
 } catch (\Exception $e) {
     $_SESSION['message'] = '<div class="notice">'.$e->getMessage().'</div>';
 }
